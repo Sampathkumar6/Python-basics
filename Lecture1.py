a2e84c5824af85aa733afe7076629a7355283b44
Sampathkumar6 <45492088+Sampathkumar6@users.noreply.github.com>
2018-12-03 12:02:55 +0530

    Add files via upload

---
+++ Lecture1.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+my_list1=[1,2,3,4]
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+my_array1=np.array(my_list1
+                  )
+
+
+# In[5]:
+
+
+my_array1
+
+
+# In[6]:
+
+
+my_list2=[11,22,33,44]
+
+
+# In[7]:
+
+
+my_list=[my_list1,my_list2]
+
+
+# In[8]:
+
+
+my_list
+
+
+# In[9]:
+
+
+my_array2=np.array(my_list)
+
+
+# In[10]:
+
+
+my_array2
+
+
+# In[11]:
+
+
+my_array2.shape
+
+
+# In[12]:
+
+
+my_array2.dtype
+
+
+# In[17]:
+
+
+np.zeros(5)  
+
+
+# In[18]:
+
+
+my_zeroes_array=np.zeros(5)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+my_zeroes_array
+
+
+# In[20]:
+
+
+my_zeroes_array.dtype
+
+
+# In[21]:
+
+
+np.ones([3,3])
+
+
+# In[22]:
+
+
+np.empty(5)
+
+
+# In[26]:
+
+
+np.eye(5)
+
+
+# In[27]:
+
+
+np.arange(5)
+
+
+# In[28]:
+
+
+np.arange(5,100,5)
+
+
+# In[ ]:
+
+
+
+
